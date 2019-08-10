2b21b8506c0c2ab099458eb86833e16ba9833ecf
rodrigo.lins <rodrigo.lins@melhorcom.com.br>
2019-08-10 11:07:16 -0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>
+          Losts project
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <p>
+          Encontre o que perdeu, ache quem perdeu...
+          </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+        <a target="_blank"  href="https://github.com/rodrigolinsbr/losts">https://github.com/rodrigolinsbr/losts</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
